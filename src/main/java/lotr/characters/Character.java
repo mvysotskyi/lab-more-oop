96408bd9ed79406940178d2be7bdf805dd01cb02
Mykola Vysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-10-07 13:04:01 +0300

    Character.java changes

--- src/main/java/lotr/characters/Character.java
+++ src/main/java/lotr/characters/Character.java
@@ -1,31 +1,30 @@
-package lotr;
+package lotr.characters;
 
 import lombok.Getter;
-import lombok.Setter;
+
 
 @Getter
-@Setter
 public abstract class Character {
     private int hp;
     private int power;
     
     public Character(int hp, int power) {
-	this.hp = hp;
-	this.power = power;
+	    this.hp = hp;
+	    this.power = power;
     }
 
     public abstract void kick(Character chr);
     
     public boolean isAlive() {
-	return hp > 0;
+	    return hp > 0;
     }
 
     public void setHp(int hp) {
-	this.hp = (hp < 0) ? 0: hp;
+	    this.hp = (hp < 0) ? 0: hp;
     }
     
     public String toString() {
-	String className = this.getClass().getSimpleName();
-	return className + "{hp=" + hp + ", power=" + power + "}";
+	    String className = this.getClass().getSimpleName();
+	    return className + "{hp=" + hp + ", power=" + power + "}";
     }
 }
