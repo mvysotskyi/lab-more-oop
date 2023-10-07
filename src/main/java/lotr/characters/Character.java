d96949ba0173da2e72103a8f78b09b9faf4d2a0d
Mykola Vysotskyi <vysotskyi.pn@ucu.edu.ua>
2023-10-07 13:01:46 +0300

    character superclass

---
+++ src/main/java/lotr/characters/Character.java
@@ -0,0 +1,31 @@
+package lotr;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Character {
+    private int hp;
+    private int power;
+    
+    public Character(int hp, int power) {
+	this.hp = hp;
+	this.power = power;
+    }
+
+    public abstract void kick(Character chr);
+    
+    public boolean isAlive() {
+	return hp > 0;
+    }
+
+    public void setHp(int hp) {
+	this.hp = (hp < 0) ? 0: hp;
+    }
+    
+    public String toString() {
+	String className = this.getClass().getSimpleName();
+	return className + "{hp=" + hp + ", power=" + power + "}";
+    }
+}
